74a49a4684460eea8fd388065e5f780a35824a54
Donnie Brody-Jackson <donnie61404@gmail.com>
2025-12-09 21:12:13 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  "projectId": "culture-rally",
+  "projectId": "ehibackend",
   "appId": "1:115324771386:web:05340ef4768be9c93ae903",
   "apiKey": "AIzaSyD_Huc9Stfz-VDoiRxSdsh41cdZX7XuffQ",
-  "authDomain": "culture-rally.firebaseapp.com",
+  "authDomain": "ehibackend.firebaseapp.com",
   "measurementId": "G-GB2211C4D1",
   "messagingSenderId": "115324771386"
 };
