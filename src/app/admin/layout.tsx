1d9dd780cfdcdf9e2a9fdee78b0169c355d343e8
Donnie Brody-Jackson <donnie61404@gmail.com>
2025-12-09 04:25:20 +0000

    Try fixing this error: `Runtime Error: useSidebar must be used within a

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
 import { AdminSidebar } from "./_components/admin-sidebar"
@@ -46,23 +45,22 @@ export default function AdminLayout({
     }
   }, [user, userLoading, firestore, router]);
 
-  if (loading || !isAuthorized) {
-    return (
-      <div className="flex min-h-screen w-full items-center justify-center">
-        <div className="animate-pulse">
-            <Logo className="h-12 w-auto text-muted-foreground" />
-        </div>
-      </div>
-    );
-  }
 
   return (
     <SidebarProvider>
-      <AdminSidebar />
-      <SidebarInset>
-        <Header />
-        {children}
-      </SidebarInset>
+        <AdminSidebar />
+        <SidebarInset>
+            <Header />
+            {loading || !isAuthorized ? (
+                 <div className="flex h-[calc(100vh-4rem)] w-full items-center justify-center">
+                    <div className="animate-pulse">
+                        <Logo className="h-12 w-auto text-muted-foreground" />
+                    </div>
+                </div>
+            ) : (
+                children
+            )}
+        </SidebarInset>
     </SidebarProvider>
   )
 }
