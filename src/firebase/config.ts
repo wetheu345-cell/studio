cb1f6d4ae786032021a8d46a0a1e2b4b6c6ef8c0
Donnie Brody-Jackson <donnie61404@gmail.com>
2025-12-09 06:46:51 +0000

    const firebaseConfig = {   apiKey: "AIzaSyD_Huc9Stfz-VDoiRxSdsh41cdZX7Xu

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "culture-rally",
-  "appId": "1:115324771386:web:05340ef4768be9c93ae903",
-  "apiKey": "AIzaSyD_Huc9Stfz-VDoiRxSdsh41cdZX7XuffQ",
-  "authDomain": "culture-rally.firebaseapp.com",
-  "measurementId": "G-S0S263JQS3",
-  "messagingSenderId": "115324771386"
+  apiKey: "AIzaSyD_Huc9Stfz-VDoiRxSdsh41cdZX7XuffQ",
+  authDomain: "culture-rally.firebaseapp.com",
+  databaseURL: "https://culture-rally-default-rtdb.firebaseio.com",
+  projectId: "culture-rally",
+  storageBucket: "culture-rally.appspot.com",
+  messagingSenderId: "115324771386",
+  appId: "1:115324771386:web:05340ef4768be9c93ae903"
 };
