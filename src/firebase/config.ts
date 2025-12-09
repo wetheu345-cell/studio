bd2ccf4da856d61ebcea46f68cdcd97d46f1174f
Donnie Brody-Jackson <donnie61404@gmail.com>
2025-12-09 17:51:45 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,8 +2,8 @@
 export const firebaseConfig = {
   "projectId": "culture-rally",
   "appId": "1:115324771386:web:05340ef4768be9c93ae903",
-  "storageBucket": "culture-rally.appspot.com",
   "apiKey": "AIzaSyD_Huc9Stfz-VDoiRxSdsh41cdZX7XuffQ",
   "authDomain": "culture-rally.firebaseapp.com",
+  "measurementId": "G-GB2211C4D1",
   "messagingSenderId": "115324771386"
 };
