b740659c819d8f9624763978d503091b4e5e7c03
Donnie Brody-Jackson <donnie61404@gmail.com>
2025-12-09 22:26:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
   "projectId": "culture-rally",
-  "appId": "1:94794273595:web:61d64a274551c62695c898",
+  "appId": "1:115324771386:web:08f05abf2e0a3c403ae903",
   "storageBucket": "culture-rally.appspot.com",
-  "apiKey": "AIzaSyAw6s-3I-g43g4jGv8d4c9Z8o7xY6y4t3E",
+  "apiKey": "AIzaSyD_Huc9Stfz-VDoiRxSdsh41cdZX7XuffQ",
   "authDomain": "culture-rally.firebaseapp.com",
-  "messagingSenderId": "94794273595"
+  "messagingSenderId": "115324771386"
 };
